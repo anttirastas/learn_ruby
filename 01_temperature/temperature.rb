b97926ce55c4c2c1717a3e1a15f837417d727a35
Antti Rastas <antti.rastas@gmail.com>
2017-03-12 15:19:41 +0200

    Solve exercise 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+# write your code here
+# one degree fahrenheit is 5/9 of one degree celsius,
+# and that the freezing point of water is 0 degrees
+# celsius but 32 degrees fahrenheit
+def ftoc(fahrenheit)
+	(fahrenheit - 32.0) * 5.0 / 9.0
+end
+
+def ctof(celsius)
+	32.0 + (celsius * 9.0 / 5.0)
+end
