222b78a750754b80a47c34ce98fcded54bc646c4
Antti Rastas <antti.rastas@gmail.com>
2017-03-13 09:08:59 +0200

    Solve exercise 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo(string)
+	string
+end
+
+def shout(string)
+	string.upcase!
+end
+
+def repeat(string, times=2)
+	times = times - 1
+	"#{string} " * times + "#{string}" 
+end
+
+def start_of_word(string, number_of_letters)
+	string.slice(0, number_of_letters)
+end
+
+def first_word(string)
+	string.split.first
+end
+
+def titleize(string)
+	string.capitalize!
+	words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+	phrase = string.split(" ").map {|word|
+		if words_no_cap.include?(word)
+			word
+		else
+			word.capitalize
+		end
+	}.join(" ")
+	phrase
+end
